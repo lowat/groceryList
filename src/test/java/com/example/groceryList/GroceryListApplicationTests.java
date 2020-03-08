b3c7e55663e5bff705ca35a328a0dc0566ff7f23
lowat <lw2014wa@gmail.com>
2020-03-08 10:02:49 -0500

    Adding project in
    Adding project in

---
+++ src/test/java/com/example/groceryList/GroceryListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groceryList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
